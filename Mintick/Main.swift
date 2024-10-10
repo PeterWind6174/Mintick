b61e96071eccceee0eb308f106bdae530b7b8121
PeterWind6174 <2123652569@qq.com>
2024-10-10 19:18:33 +0800

    Improvements

--- Mintick/Main.swift
+++ Mintick/Main.swift
@@ -11,8 +11,42 @@ struct Main: View {
     var body: some View {
         NavigationView(){
             ScrollView(){
-                Text("Hello")
-                Spacer()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.blue)
+                        .frame(height: 100)
+                        .padding()
+                    
+                    HStack {
+                        Text("HH:MM:SS")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            //.padding(.leading, 20) // 文字左对齐
+                        
+                        Spacer() // 添加间隔符，让按钮靠右对齐
+                        
+                        // 第一个圆形按钮
+                        Button(action: {
+                            //Actions
+                        }) {
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 40, height: 40)
+                        }
+                        
+                        Spacer()
+                        
+                        // 第二个圆形按钮
+                        Button(action: {
+                            //Actions
+                        }) {
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 40, height: 40)
+                        }
+                    }
+                    .frame(width: 280) // 限制 HStack 的宽度，保证布局美观
+                }
             }
             .navigationBarTitle("今天的日期")
         }
