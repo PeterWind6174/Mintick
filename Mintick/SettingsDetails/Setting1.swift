56963fc642197328626793045508d54adc4b7172
zcy0602 <117595784+zcy0602@users.noreply.github.com>
2024-10-26 00:13:58 +0800

    Add files via upload

---
+++ Mintick/SettingsDetails/Setting1.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct Setting1: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    Setting1()
+}
